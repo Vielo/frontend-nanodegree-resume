36e7f4fc093da7bbb5a71f41881aa7ebe1242da9
Vielo <radoslawjakubiak@gmail.com>
2017-02-28 20:38:18 +0100

    Add name and role in resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,14 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Radosław Jakubiak");
+ 
+ var name = "Radosław Jakubiak";
+ var role = "Community Specialist and Web Developer in training";
+ 
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+
