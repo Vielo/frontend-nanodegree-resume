cdfe07a6ce99a10b387167dc16e149e04e36c8dd
Vielo <radoslawjakubiak@gmail.com>
2017-03-04 19:27:02 +0100

    Add bio items

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,4 +11,25 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
+var bio = {
+	"name": "Radosław Jakubiak",
+	"role": "Community Specialist and Web Developer in training",
+	"contactMail": "radoslawjakubiak@gmail.com",
+	"contactGithub": "https://github.com/Vielo/",
+	"pictureUrl": "images/photo.jpg",
+	"welcomeMsg": "Hello!",
+	"skills": ["HTML", "CSS", "SASS", "JavaScript", "Community Management",]
+};
 
+var formattedContactMail = HTMLemail.replace("%data%", bio.contactMail);
+var formattedContactGithub = HTMLgithub.replace("%data%", bio.contactGithub);
+var formattedPictureurl = HTMLbioPic.replace("%data%", bio.pictureUrl);
+var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+
+$("#header").append(formattedContactMail);
+$("#header").append(formattedContactGithub);
+$("#header").append(formattedPictureurl);
+$("#header").append(formattedWelcomeMsg);
+$("#header").append(HTMLskillsStart);
+$("#header").append(formattedSkills);
